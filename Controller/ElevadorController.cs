3c432a14a019877f3d49cfa4b66259acb7b3f8a3
junglemaodev <adenilton.barroso@gmail.com>
2022-01-09 16:20:21 -0400

    Implementando o método construtor da classe ElevadorController

--- Controller/ElevadorController.cs
+++ Controller/ElevadorController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetoElevador.Model;
+using ProjetoElevador.View;
 
 namespace ProjetoElevador.Controller
 {
-    internal class ElevadorController
+    public class ElevadorController
     {
+        private Elevador _elevador = new Elevador();
+        private List<Pessoa> _pessoas = new List<Pessoa>();
 
+        public ElevadorController()
+        {
+            PainelElevador painel  = new PainelElevador(_elevador);
+        }
     }
 }
